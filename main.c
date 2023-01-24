2affa35ad59ed6401ab4960855cbe6d6806168ea
Feben9 <feben7351@gmail.com>
2023-01-24 11:52:46 -0800

    Testing

--- main.c
+++ main.c
@@ -67,9 +67,9 @@ int main(void)
 
 														    printf("Len:[%d]\n", len2);
 
-														        _printf("Unknown:[%r]\n");
+														        _printf("Unknown:[%%]\n");
 
-															    printf("Unknown:[%r]\n");
+															    printf("Unknown:[%%]\n");
 
 															        return (0);
 
